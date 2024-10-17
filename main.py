3a1e8b6ee1279ec74afa204d1156ecbb124f7031
Stephanethr <stethiry9@gmail.com>
2024-10-17 09:06:51 +0200

    add timestamp in file name

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import torch
 from diffusers import StableDiffusionPipeline
 from PIL import Image
 import platform
+from datetime import datetime  # Importer le module datetime
 
 # Vérifier le système d'exploitation
-
 os_name = platform.system()
 if os_name == "Windows":
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -15,17 +15,19 @@ model_id = "CompVis/stable-diffusion-v1-4"
 pipe = StableDiffusionPipeline.from_pretrained(model_id)
 pipe = pipe.to(device)
 
-
 def generate_image(prompt):
     with torch.no_grad():
         image = pipe(prompt).images[0]
     return image
 
-
-prompt = "a pepperoni pizza with a lot of cheese"
+prompt = "a paiting of a mountain landscape with a river in the foreground"
 
 generated_image = generate_image(prompt)
 
 generated_image.show()
 
-generated_image.save("generated_image.png")
+# Ajouter un timestamp au nom de l'image
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = f"generated_image_{timestamp}.png"
+
+generated_image.save(filename)
