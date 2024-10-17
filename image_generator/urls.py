b7b28d7a0dd2c7e91a44a390ffddb92c4f3fb37f
Stephanethr <stethiry9@gmail.com>
2024-10-17 09:47:18 +0200

    Connexion front et back

--- image_generator/urls.py
+++ image_generator/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import generate_image_view
+from .views import index, generate_image_view
 
 urlpatterns = [
-    path('generate/', generate_image_view, name='generate_image'),
+    path('', index, name='index'),  # Route pour l'interface utilisateur
+    path('generate/', generate_image_view, name='generate_image'),  # API pour générer l'image
 ]
--- image_generator/views.py
+++ image_generator/views.py
@@ -1,34 +1,41 @@
 import torch
 from diffusers import StableDiffusionPipeline
 from PIL import Image
-import platform
-from django.http import JsonResponse
-from datetime import datetime
 import base64
 from io import BytesIO
+from django.http import JsonResponse
+from django.shortcuts import render
 
 
-os_name = platform.system()
-device = "cuda" if os_name == "Windows" and torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+# Fonction pour afficher la page index
+def index(request):
+    return render(request, 'image_generator/index.html')
 
-model_id = "CompVis/stable-diffusion-v1-4"
-pipe = StableDiffusionPipeline.from_pretrained(model_id)
-pipe = pipe.to(device)
 
-def generate_image(prompt):
-    with torch.no_grad():
-        image = pipe(prompt).images[0]
-    return image
-
+# Fonction pour générer l'image via l'API
 def generate_image_view(request):
     prompt = request.GET.get('prompt', '')
+
     if not prompt:
-        return JsonResponse({'error': 'Prompt not provided'}, status=400)
+        return JsonResponse({'error': 'No prompt provided'}, status=400)
 
-    image = generate_image(prompt)
-    
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    # Charger et utiliser le modèle de génération d'image
+    try:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        model_id = "CompVis/stable-diffusion-v1-4"
+        pipe = StableDiffusionPipeline.from_pretrained(model_id)
+        pipe = pipe.to(device)
 
-    return JsonResponse({'image_data': img_str})  
+        # Générer l'image
+        with torch.no_grad():
+            image = pipe(prompt).images[0]
+
+        # Sauvegarder l'image en mémoire et la convertir en base64
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+        return JsonResponse({'image_data': img_str})
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
